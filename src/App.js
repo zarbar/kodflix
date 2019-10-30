91f7b302c5f0cf7f277a262f5d9d58e0e7d7eaa0
Zara <48896844+zarbar@users.noreply.github.com>
2019-10-30 09:28:03 +0000

    stages 2 and 3 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import killerTomato from './killer-tomato.jpg';
+// import { avatar, biglittle, curb, glow, naked, rupaul } from './images';
+import avatar from './images/avatar.jpg';
+import biglittle from './images/biglittle.jpg';
+import curb from './images/curb.jpg';
+import GLOW from './images/GLOW.jpg';
+import rupaul from './images/rupaul.png';
+import naked from './images/naked.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Welcome to Kodflix</h1>
+      <img src={killerTomato} alt='attack of the killer tomatoes' />
+      <div class="container">
+        <div className="col">
+          <img className="tv-show-poster" src={avatar} alt='avatar the last airbender' />
+        </div>
+        <div className="col">
+          <img className="tv-show-poster" src={biglittle} alt='Big Little Lies' />
+        </div>
+        <div className="col">
+          <img className="tv-show-poster" src={curb} alt='Curb your Enthusiasm' />
+        </div>
+      </div >
+      <div class="container">
+        <div className="col">
+          <img className="tv-show-poster" src={GLOW} alt='GLOW' />
+        </div>
+        <div className="col">
+          <img className="tv-show-poster" src={naked} alt='Naked Director' />
+        </div>
+        <div className="col">
+          <img className="tv-show-poster" src={rupaul} alt='rupaul' />
+        </div>
+      </div >
+    </div >
   );
 }
 
