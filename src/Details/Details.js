0a668d92ea8121dd97b3f5b31bc09d2e121c11fa
Zara <48896844+zarbar@users.noreply.github.com>
2019-11-12 12:24:16 +0000

    #13 complete

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 export default function Details(props) {
+    const [message, setMessage] = useState('');
+
+    React.useEffect(
+        () => {
+            const timedMsg = setTimeout(() => setMessage('Coming Soon! :-P'), 3000)
+            return () => clearTimeout(timedMsg)
+        }, []
+    )
+
     return (
         <>
-            This is the movie details page<br />
+            <h1>This is the details page<br /></h1>
             <Link to='/'>Go Back</Link>
+            <br />
+            <div>{message}</div>
+
+
         </>
     );
 } 
