94369b38d5754cfc077d5e94788adce700a525fd
Zara <48896844+zarbar@users.noreply.github.com>
2019-11-19 11:06:20 +0000

    solved useEffect error on details page

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,26 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Redirect } from "react-router-dom";
-import ShowList from '../ShowList/ShowList';
+import ShowList from '../ShowList/ShowList.js';
 import './Details.css';
 
 export default function Details(props) {
-    let { details } = useParams();
+    const { details } = useParams();
     const [thisShow, setThisShow] = useState('show title here');
     const [synopsis, setSynopsis] = useState('show title here');
     const [showImage, setShowImage] = useState('');
     const [message, setMessage] = useState('');
 
-    let findShow = () => {
-        let show = ShowList().find(show => show.id === details);
-        setThisShow(show ? show.title : undefined);
-        setSynopsis(show ? show.synopsis : 'Synopsis coming soon');
-        setShowImage(show ? show.image : '');
-    };
-
     useEffect(
         () => {
-            findShow();
-        }, []
+            let show = ShowList().find(show => show.id === details);
+            setThisShow(show ? show.title : undefined);
+            setSynopsis(show ? show.synopsis : 'Synopsis coming soon');
+            setShowImage(show ? show.image : '');
+        }, [details]
     )
 
     React.useEffect(
