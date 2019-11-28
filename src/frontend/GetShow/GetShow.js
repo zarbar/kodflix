4a6effcf7b4103b0aad296303477918b789a7f5c
Zara <48896844+zarbar@users.noreply.github.com>
2019-11-28 15:09:21 +0000

    backend ready

--- src/frontend/GetShow/GetShow.js
+++ src/frontend/GetShow/GetShow.js
@@ -1,17 +1,13 @@
 import Show from '../DisplayShow/DisplayShow';
 import React, { useState, useEffect } from 'react';
-// import ShowList from '../ShowList/ShowList';
 
 export default function GetShow() {
     const [shows, setShows] = useState('');
 
     useEffect(() => {
         fetch('/rest/shows')
-            // displays as a string as shown at: http://localhost:3002/rest/shows  - a stringified version of the return function in the backend shows.js function
             .then(res => res.json())
-            //we are converting this string back into an JS object (which in this case is an array, as that was in the original return function)
             .then(json => setShows(json))
-        //setting this array as the shows state variable
     }, [])
 
     if (shows) {
@@ -34,25 +30,17 @@ export default function GetShow() {
 }
 
 
-{/* {shows
-                .map(show => (
-                    <Show image={show.image}
-                        title={show.title}
-                        id={show.id}
-                        key={show.id}
-                    />
-                ))} */}
+// useEffect(() => {
+//     fetch('/rest/shows')
 
+        // displays as a string as shown at: http://localhost:3002/rest/shows  - a stringified version of the return function in the backend shows.js function
 
+        // .then(res => res.json())
 
+        //we are converting this string back into an JS object (which in this case is an array, as that was in the original return function)
 
+        // .then(json => setShows(json))
 
+    //setting this array as the shows state variable
 
-// {ShowList()
-//     .map(show => (
-//         <Show image={show.image}
-//             title={show.title}
-//             id={show.id}
-//             key={show.id}
-//         />
-//     ))}
+// }, [])
