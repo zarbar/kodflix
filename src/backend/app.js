7da46202b7b1184989dab83390c92dca18a0ea89
Zara <48896844+zarbar@users.noreply.github.com>
2019-11-29 17:19:23 +0000

    worked on #23

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3003;
 const shows = require('./shows');
 
-app.use(express.static(path.join(__dirname, '../../build', 'index.html')));
-
-app.get('/', (req, res) => res.send('Hello big  Universe!'));
-
 app.get('/rest/shows', (req, res) => res.send(shows()));
+
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))   
