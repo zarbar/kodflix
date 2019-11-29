3e6abc36ab0d78fe4d3d64d74ace4185deb277e1
Zara <48896844+zarbar@users.noreply.github.com>
2019-11-29 09:41:19 +0000

    build folder created

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 3001;
 const shows = require('./shows');
+
+app.use(express.static(path.join(__dirname, '../../build', 'index.html')));
 
 app.get('/', (req, res) => res.send('Hello big  Universe!'));
 
