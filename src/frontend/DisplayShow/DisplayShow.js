de76e62aa7639de9348bba0a928e1de0d27eb58d
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-17 15:11:52 +0000

    removed unnecessary variable/props on DisplayShow.js

--- src/frontend/DisplayShow/DisplayShow.js
+++ src/frontend/DisplayShow/DisplayShow.js
@@ -3,11 +3,9 @@ import { Link } from "react-router-dom";
 import './DisplayShow.css';
 
 export default function Show(props) {
-    let imageSrc = `../images/${props.id}.jpg`;
-
     return (
         <div className="image-container">
-            <Link to={props.id} show={props} imageSrc={imageSrc}>
+            <Link to={props.id} show={props}>
                 <img src={require(`../images/${props.id}.jpg`)} alt={props.title} />
                 <div className="tv-show-overlay">
                     <h2>{props.title}</h2>
